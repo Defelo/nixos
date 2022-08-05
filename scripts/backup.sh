854f5931e6069b9cf292bcecaae1318ba95d57b0
Defelo <elodef42@gmail.com>
2022-08-05 16:01:25 +0200

    [scripts] Add progress flag to backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 REPO=$(sudo cat /etc/borgmatic/repo)
-BORG_PASSPHRASE=$(pass show Borg/$REPO) sudo --preserve-env=BORG_PASSPHRASE borgmatic -v1 --files --stats
+BORG_PASSPHRASE=$(pass show Borg/$REPO) sudo --preserve-env=BORG_PASSPHRASE borgmatic -v1 --files --stats --progress
