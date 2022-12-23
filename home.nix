eb5cf4220626b4a8e3e2fca39578326afdc2963f
Defelo <elodef42@gmail.com>
2022-12-23 17:24:47 +0100

    Add wallpaper

--- home.nix
+++ home.nix
@@ -19,6 +19,8 @@
     git-crypt
     neofetch
     lxappearance
+    feh
+    gnome.eog
   ];
 
   home.sessionPath = [ "$HOME/.local/bin" "$HOME/.cargo/bin" ];
@@ -142,6 +144,7 @@
     scriptPath = ".hm-xsession";
     initExtra = ''
       xrandr --output Virtual-1 --mode 1920x1080 --rate 60
+      ${pkgs.feh}/bin/feh --bg-scale ${./wallpaper.png}
     '';
     windowManager.i3 = {
       enable = true;
