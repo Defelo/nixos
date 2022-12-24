0805eff649564caccd76e1dceab0e578aa64e63d
Defelo <elodef42@gmail.com>
2022-12-24 15:11:34 +0100

    Improve i3 config

--- home.nix
+++ home.nix
@@ -184,6 +184,14 @@
         keybindings = lib.mkOptionDefault {
           "${modifier}+Return" = "exec alacritty";
           "${modifier}+Tab" = "workspace back_and_forth";
+          "${modifier}+h" = "focus left";
+          "${modifier}+j" = "focus down";
+          "${modifier}+k" = "focus up";
+          "${modifier}+l" = "focus right";
+          "${modifier}+Shift+h" = "move left";
+          "${modifier}+Shift+j" = "move down";
+          "${modifier}+Shift+k" = "move up";
+          "${modifier}+Shift+l" = "move right";
         };
       };
     };
