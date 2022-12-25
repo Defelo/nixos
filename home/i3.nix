361d5136012c25a8499c9b30330d0bd31651350b
Defelo <elodef42@gmail.com>
2022-12-25 07:28:28 +0100

    Improve i3 config

---
+++ home/i3.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, ... }:
+
+let mod = "Mod4"; in
+{
+  enable = true;
+  package = pkgs.i3-gaps;
+  config = {
+    modifier = mod;
+    keybindings = lib.mkOptionDefault {
+      "${mod}+Return" = "exec alacritty";
+      "${mod}+d" = ''exec "rofi -combi-modi drun,ssh,run -modi combi -show combi -show-icons"'';
+
+      "${mod}+Tab" = "workspace back_and_forth";
+
+      "${mod}+h" = "focus left";
+      "${mod}+j" = "focus down";
+      "${mod}+k" = "focus up";
+      "${mod}+l" = "focus right";
+
+      "${mod}+Shift+h" = "move left";
+      "${mod}+Shift+j" = "move down";
+      "${mod}+Shift+k" = "move up";
+      "${mod}+Shift+l" = "move right";
+    };
+  };
+}
--- home/x11.nix
+++ home/x11.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, ... } @ inputs:
 
 {
   xsession = {
@@ -8,24 +8,6 @@
       xrandr --output Virtual-1 --mode 1920x1080 --rate 60
       ${pkgs.feh}/bin/feh --bg-scale ${../wallpaper.png}
     '';
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      config = rec {
-        modifier = "Mod4";
-        keybindings = lib.mkOptionDefault {
-          "${modifier}+Return" = "exec alacritty";
-          "${modifier}+Tab" = "workspace back_and_forth";
-          "${modifier}+h" = "focus left";
-          "${modifier}+j" = "focus down";
-          "${modifier}+k" = "focus up";
-          "${modifier}+l" = "focus right";
-          "${modifier}+Shift+h" = "move left";
-          "${modifier}+Shift+j" = "move down";
-          "${modifier}+Shift+k" = "move up";
-          "${modifier}+Shift+l" = "move right";
-        };
-      };
-    };
+    windowManager.i3 = import ./i3.nix inputs;
   };
 }
