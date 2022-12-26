364ac2d316a338a70ae1b0a50b723b7f8b203b85
Defelo <elodef42@gmail.com>
2022-12-27 00:34:43 +0100

    Add alacritty config

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,5 +1,20 @@
 {...}: {
   programs.alacritty = {
     enable = true;
+    settings = {
+      # env.TERM = "xterm-256color";
+      window = {
+        opacity = 0.8;
+        title = "Alacritty";
+        dynamic_title = false;
+      };
+      font = {
+        normal.family = "MesloLGS NF";
+        bold.family = "MesloLGS NF";
+        italic.family = "MesloLGS NF";
+        bold_italic.family = "MesloLGS NF";
+        size = 7;
+      };
+    };
   };
 }
