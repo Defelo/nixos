a87fe1a068409446f0085ca5aa367423bedb6412
Defelo <elodef42@gmail.com>
2022-12-27 20:23:07 +0100

    Auto start alacritty

--- home/x11.nix
+++ home/x11.nix
@@ -9,6 +9,7 @@
     initExtra = ''
       xrandr --output Virtual-1 --mode 1920x1080 --rate 60
       ${pkgs.feh}/bin/feh --bg-scale ${../wallpaper.png}
+      alacritty &
     '';
     windowManager.i3 = import ./i3.nix inputs;
   };
