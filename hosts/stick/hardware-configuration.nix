a296a5bfef6dfee0be66008be32bc079f0a8ea63
Defelo <elodef42@gmail.com>
2022-12-29 20:54:28 +0100

    Add swap

--- hosts/stick/hardware-configuration.nix
+++ hosts/stick/hardware-configuration.nix
@@ -25,7 +25,12 @@
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/dev/nixos/swap";
+      priority = 0;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
