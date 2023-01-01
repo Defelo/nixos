08e8d38a9f174244ab34a8a64360e12f0a56fa72
Defelo <elodef42@gmail.com>
2023-01-01 23:41:09 +0100

    Enable suspend-then-hibernate

--- hosts/stick/power.nix
+++ hosts/stick/power.nix
@@ -3,4 +3,13 @@
     enable = true;
   };
   powerManagement.powertop.enable = true;
+  services.logind = {
+    lidSwitch = "suspend-then-hibernate";
+    extraConfig = ''
+      HandlePowerKey=hibernate
+    '';
+  };
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30m
+  '';
 }
