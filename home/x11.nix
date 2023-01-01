e1607dfcd76ba114e2b90cd56a9d5527f4967fc2
Defelo <elodef42@gmail.com>
2023-01-01 23:40:16 +0100

    Lock screen on suspend/hibernate

--- home/x11.nix
+++ home/x11.nix
@@ -9,6 +9,7 @@
     initExtra = ''
       xrandr --output Virtual-1 --mode 1920x1080 --rate 60
       ${pkgs.feh}/bin/feh --bg-scale ${../wallpaper.png}
+      ${pkgs.xss-lock}/bin/xss-lock --transfer-sleep-lock -- ${pkgs.i3lock-fancy}/bin/i3lock-fancy -pt "" &
       brave &
       thunderbird &
       discordcanary &
