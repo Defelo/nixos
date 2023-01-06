4c07256f868974a713caade5769536e4f80cc9c1
Defelo <elodef42@gmail.com>
2023-01-06 22:20:29 +0100

    Enable repl-flake experimental nix feature

--- system/common.nix
+++ system/common.nix
@@ -33,7 +33,7 @@
     };
     settings = {
       auto-optimise-store = true;
-      experimental-features = ["nix-command" "flakes"];
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
     };
     registry = {
       nixpkgs = {
