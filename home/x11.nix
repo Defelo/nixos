94d51378a7847681cb179edf98d7c53e6732264c
Defelo <elodef42@gmail.com>
2023-01-07 22:39:18 +0100

    Remove useless xrandr setup

--- home/x11.nix
+++ home/x11.nix
@@ -7,7 +7,6 @@
     enable = true;
     scriptPath = ".xinitrc";
     initExtra = ''
-      xrandr --output Virtual-1 --mode 1920x1080 --rate 60
       ${pkgs.feh}/bin/feh --bg-scale ${../wallpaper.png}
       ${pkgs.xss-lock}/bin/xss-lock --transfer-sleep-lock -- ${pkgs.i3lock-fancy}/bin/i3lock-fancy -pt "" &
       PATH=${pkgs.xfce.xfce4-clipman-plugin}/bin:$PATH xfce4-clipman &
