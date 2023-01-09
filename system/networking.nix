63b74021bec13a317a1cdcbfdbc18397037dcd97
Defelo <elodef42@gmail.com>
2023-01-09 17:33:36 +0100

    Disable rpfilter for wireguard

--- system/networking.nix
+++ system/networking.nix
@@ -15,6 +15,19 @@
     ];
     allowedUDPPorts = [];
     trustedInterfaces = ["docker0"];
+
+    # disable rpfilter for wireguard
+    # if packets are still dropped, they will show up in dmesg
+    logReversePathDrops = true;
+    # wireguard trips rpfilter up
+    extraCommands = ''
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+    '';
+    extraStopCommands = ''
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+    '';
   };
 
   environment.etc = with pkgs.lib.attrsets;
