4acfda7471eaab5fb4792a61bdd0b38366a89c0c
Defelo <elodef42@gmail.com>
2023-01-09 22:05:35 +0100

    Improve picom config

--- home/picom.nix
+++ home/picom.nix
@@ -1,12 +1,28 @@
-{...}: {
+{pkgs, ...}: {
   services.picom = {
     enable = true;
-    backend = "glx";
+    backend = "xr_glx_hybrid";
+    extraArgs = ["--transparent-clipping"];
     fade = true;
     fadeDelta = 4;
     fadeSteps = [0.055 0.055];
     shadow = true;
     vSync = true;
+    activeOpacity = 1;
     inactiveOpacity = 0.9;
+    opacityRules = let
+      rules = {
+        Alacritty = 95;
+        thunderbird = 90;
+        discord = 90;
+        Element = 90;
+        TelegramDesktop = 95;
+        obsidian = 95;
+      };
+    in
+      pkgs.lib.attrsets.mapAttrsToList (key: value: "${toString value}:class_g = '${key}'") rules;
+    settings = {
+      corner-radius = 4;
+    };
   };
 }
