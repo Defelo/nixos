e72a93d9fb2b5f7ecf363c6c7bae95753a34aa6e
Defelo <elodef42@gmail.com>
2023-01-10 20:13:49 +0100

    Disable screensaver

--- home/x11.nix
+++ home/x11.nix
@@ -7,10 +7,24 @@
     enable = true;
     scriptPath = ".xinitrc";
     initExtra = ''
+      # disable screensaver
+      xset s off
+      xset -dpms
+      xset s noblank
+
+      # set wallpaper
       ${pkgs.feh}/bin/feh --bg-scale ${../wallpapers/nix-snowflake-dark.png}
+
+      # lock screen on suspend/hibernate
       ${pkgs.xss-lock}/bin/xss-lock --transfer-sleep-lock -- ${pkgs.i3lock-fancy}/bin/i3lock-fancy -pt "" &
+
+      # start clipman
       PATH=${pkgs.xfce.xfce4-clipman-plugin}/bin:$PATH xfce4-clipman &
+
+      # fix keyboard layout
       setxkbmap de nodeadkeys
+
+      # start some applications
       brave &
       thunderbird &
       discordcanary &
