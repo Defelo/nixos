c9411c7bd904af3aa01ac19384b7787460fe9d27
Defelo <elodef42@gmail.com>
2023-01-11 11:44:36 +0100

    Fix rofipass.sh

--- scripts/rofipass.sh
+++ scripts/rofipass.sh
@@ -3,7 +3,7 @@
 shopt -s globstar
 
 cd $PASSWORD_STORE_DIR
-if x=$((for f in **/*; do echo "${f%.gpg}"; done) | rofi -dmenu -no-custom); then
+if x=$((for f in $(find * -type f -name '*.gpg'); do echo "${f%.gpg}"; done) | rofi -dmenu -no-custom); then
     if PASSWORD_STORE_CLIP_TIME=10 pass show --clip "$x"; then
         dunstify -t 10000 "Password Store" "copied '$x' to clipboard"
     fi
