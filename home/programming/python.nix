fc8bec796faa488efa1c2a7901162f91a4058d99
Defelo <elodef42@gmail.com>
2023-01-16 10:56:15 +0100

    Add poetry

--- home/programming/python.nix
+++ home/programming/python.nix
@@ -8,5 +8,17 @@
         scipy
         jupyterlab
       ]))
+    poetry
+    (with python310.pkgs;
+      buildPythonApplication rec {
+        pname = "poethepoet";
+        version = "0.18.1";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-XzVmsUwvXczfvDuybwCWAGs43AucdL1PjdHrp7Din2o=";
+        };
+        propagatedBuildInputs = [tomli pastel];
+        doCheck = false;
+      })
   ];
 }
