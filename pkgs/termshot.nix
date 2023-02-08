677c79e52e9bd1e1186d9c77ef680a499efd3b82
Defelo <elodef42@gmail.com>
2023-02-08 16:44:05 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/c43d4a3d6d9ef8ddbe2438362f5c775b4186000b' (2023-02-05)
      → 'github:nix-community/home-manager/e631d78ddfbf808fd1cfc4d79039a1b3acda5bed' (2023-02-08)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/0591d6b57bfeb55dfeec99a671843337bc2c3323' (2023-02-04)
      → 'github:NixOS/nixpkgs/fab09085df1b60d6a0870c8a89ce26d5a4a708c2' (2023-02-07)
    • Updated input 'nixpkgs-small':
        'github:NixOS/nixpkgs/8030c64577a7973d07537e2bb446c14ccedaa14c' (2023-02-06)
      → 'github:NixOS/nixpkgs/f30a2734341993614edf1419829c03e2f63e3f4b' (2023-02-07)
    • Updated input 'termshot':
        'github:homeport/termshot/ad2cfa6b86da301e880584ab53af91a0e1484b3c' (2023-01-31)
      → 'github:homeport/termshot/32982c83d8958a5139926352584e9ee9802956ba' (2023-02-08)

--- pkgs/termshot.nix
+++ pkgs/termshot.nix
@@ -8,5 +8,5 @@ buildGoModule rec {
   pname = "termshot";
   version = "0.2.5";
   src = termshot;
-  vendorHash = "sha256-47AVgmGXENUzJP8ooosawCZFbizfggRM4juNiKIz9ws";
+  vendorHash = "sha256-hdez+FjYprXOsASjqerC1WR4+zPk+S61m2G5c55O8wU=";
 }
