72ff6736f94e939c1ede6875e74301aefffdd171
Defelo <elodef42@gmail.com>
2023-02-11 11:58:56 +0100

    Set --delete-older-than to 3d for nix gc

--- system/common.nix
+++ system/common.nix
@@ -29,7 +29,7 @@
     gc = {
       automatic = true;
       dates = "daily";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 3d";
     };
     settings = {
       auto-optimise-store = true;
