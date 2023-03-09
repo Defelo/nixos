0ceabf4998fd4034a76ad2180bc507a0b6359271
Defelo <elodef42@gmail.com>
2023-03-09 23:11:55 +0100

    Use sparse registries

--- home/programming/rust.nix
+++ home/programming/rust.nix
@@ -16,6 +16,9 @@
       [target.x86_64-unknown-linux-gnu]
       linker = "${pkgs.clang_14}/bin/clang"
       rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
+
+      [registries.crates-io]
+      protocol = "sparse"
     '';
     target = ".cargo/config.toml";
   };
