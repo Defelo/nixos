db7e220700e1227740f0ed987f7e69c17f1c57d0
Defelo <elodef42@gmail.com>
2023-04-29 23:40:09 +0200

    Use sops-nix for ssh config

--- home/sops.nix
+++ home/sops.nix
@@ -7,5 +7,7 @@
   sops = {
     defaultSopsFile = ../secrets/default.yml;
     age.keyFile = "${conf.home}/.config/sops/age/keys.txt";
+    defaultSymlinkPath = "/run/user/${toString conf.uid}/secrets";
+    defaultSecretsMountPoint = "/run/user/${toString conf.uid}/secrets.d";
   };
 }
