ff89415aae0478d4f543d9ad08080fc0e2fb883e
Defelo <elodef42@gmail.com>
2023-05-04 22:46:12 +0200

    Update sea-orm-cli

--- pkgs/sea-orm-cli.nix
+++ pkgs/sea-orm-cli.nix
@@ -5,14 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "sea-orm-cli";
-  version = "0.11.0";
+  version = "0.11.3";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-nFSouV23t0+CCvBXT/WhaTGoFtABnt9KjF9U0kb+zqI=";
+    sha256 = "sha256-VRSdPsjRubJOsjdAxdnFCM9VmAVwGkXDvpXT4GF2jxY=";
   };
 
-  cargoSha256 = "sha256-qJ+G5+KyoNsl3VKsHDdFD6l1MagqHd7N7TahK7B5ky8=";
+  cargoSha256 = "sha256-4lPtj11Gc+0r2WQT8gx8eX+YK5L+HnUBR0w6pm3VlRQ=";
 
   buildNoDefaultFeatures = true;
   buildFeatures = ["codegen" "cli" "runtime-async-std-rustls" "async-std"];
