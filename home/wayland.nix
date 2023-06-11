dc5881dea8f28e2e4e50b889d3940cad876e6374
Defelo <elodef42@gmail.com>
2023-06-11 22:58:59 +0200

    Increase clipboard history size

--- home/wayland.nix
+++ home/wayland.nix
@@ -23,6 +23,7 @@
 in {
   home.packages = with pkgs; [
     wl-clipboard
+    clipman
   ];
 
   wayland.windowManager.sway = {
@@ -278,5 +279,21 @@ in {
     };
   };
 
-  services.clipman.enable = true;
+  # https://github.com/nix-community/home-manager/blob/master/modules/services/clipman.nix
+  systemd.user.services.clipman = {
+    Unit = {
+      Description = "Clipboard management daemon";
+      PartOf = ["graphical-session.target"];
+      After = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste -t text --watch ${pkgs.clipman}/bin/clipman store --max-items=200";
+      ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+      Restart = "on-failure";
+      KillMode = "mixed";
+    };
+
+    Install.WantedBy = ["graphical-session.target"];
+  };
 }
