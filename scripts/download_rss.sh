d0f62b659e5d8b2460b86f458c1fe12b1e12c25b
Defelo <elodef42@gmail.com>
2023-07-22 18:52:06 +0200

    Update rss streams

--- scripts/download_rss.sh
+++ scripts/download_rss.sh
@@ -12,11 +12,13 @@ download_from_rss() {
     wget --continue -O "$name" "$url"
 }
 
+future=$3
 declare -A urls=(
     ["x3"]="http://www.ndr.de/fernsehen/sendungen/extra_3/video-podcast/extradrei196_version-hq.xml"
-    ["hs"]="https://mediathekviewweb.de/feed?query=heute-show%20!ZDF&future=false"
-    ["zmr"]="https://mediathekviewweb.de/feed?query=zdf%20magazin%20royale%20!ZDF&future=false"
-    ["m"]="https://mediathekviewweb.de/feed?query=!zdf%20wir%20sind%20die%20meiers&future=false"
+    ["hs"]="https://mediathekviewweb.de/feed?query=heute-show%20!ZDF&future=$future"
+    ["zmr"]="https://mediathekviewweb.de/feed?query=zdf%20magazin%20royale%20!ZDF&future=$future"
+    ["m"]="https://mediathekviewweb.de/feed?query=!zdf%20wir%20sind%20die%20meiers&future=$future"
+    ["zcs"]="https://mediathekviewweb.de/feed?query=!zdf%20zdf%20comedy%20sommer&future=$future"
 )
 
 download_from_rss "${urls[$1]}" "$1" ${2:-0}
