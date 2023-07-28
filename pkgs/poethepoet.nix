9bc64eb4a586063c517d9eecc1ada72fd212cbcf
Defelo <elodef42@gmail.com>
2023-07-28 18:10:40 +0200

    Update poethepoet

--- pkgs/poethepoet.nix
+++ pkgs/poethepoet.nix
@@ -2,11 +2,12 @@
 with python311.pkgs;
   buildPythonApplication rec {
     pname = "poethepoet";
-    version = "0.18.1";
+    version = "0.21.1";
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-XzVmsUwvXczfvDuybwCWAGs43AucdL1PjdHrp7Din2o=";
+      sha256 = "sha256-OddU9TcUVFojz8lLEi3q81SCGLbjW0U8MJc8VZhxnp8=";
     };
-    propagatedBuildInputs = [tomli pastel];
+    format = "pyproject";
+    propagatedBuildInputs = [poetry-core tomli pastel];
     doCheck = false;
   }
