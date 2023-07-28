93ddf5414f7e028dcb01647645e57f47aa1d0393
Defelo <elodef42@gmail.com>
2023-07-28 18:15:12 +0200

    Update termshot

--- pkgs/termshot.nix
+++ pkgs/termshot.nix
@@ -1,17 +1,16 @@
 {
-  stdenv,
   fetchFromGitHub,
   buildGoModule,
   ...
 }:
 buildGoModule rec {
   pname = "termshot";
-  version = "0.2.5";
+  version = "0.2.7";
   src = fetchFromGitHub {
     owner = "homeport";
     repo = pname;
-    rev = "v0.2.5";
-    sha256 = "sha256-fAWxdXSuQQz04ZmU6S9WWrGYDx0/u20C1rDljY85Xfc=";
+    rev = "v${version}";
+    sha256 = "sha256-Sxp6abYq0MrqtqDdpffSBdZB3/EyIMF9Ixsc7IgW5hI=";
   };
-  vendorHash = "sha256-NIJ6LkbHpHtcANj+hRuUA/Da6mkIrc3O0+fyuIJ67ZU=";
+  vendorHash = "sha256-jzDbA1iN+1dbTVgKw228TuCV3eeAVmHFDiHd2qF/80E=";
 }
