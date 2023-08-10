e42b9940dc4afcc9b20eb70c4f4047923f27c36a
Defelo <elodef42@gmail.com>
2023-08-10 12:01:26 +0200

    Fix rofipass.sh

--- home/scripts/rofipass.sh
+++ home/scripts/rofipass.sh
@@ -7,6 +7,7 @@ if x=$((for f in $(find * -type f -name '*.gpg'); do echo "${f%.gpg}"; done) | r
     password=$(pass show "$x" | head -1)
 
     # prevent clipman from storing the password
+    [[ -f ~/.local/share/clipman.json ]] || echo -n '[]' > ~/.local/share/clipman.json
     chmod u-w ~/.local/share/clipman.json
     wl-copy -n <<< "$password"
     (sleep 1; chmod u+w ~/.local/share/clipman.json)
