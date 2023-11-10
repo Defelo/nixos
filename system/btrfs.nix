8499f48a73212146866ccea1fb65b4b4d57ed6e2
Defelo <elodef42@gmail.com>
2023-11-10 18:01:20 +0100

    Reduce number of snapshots to keep for @cache

--- system/btrfs.nix
+++ system/btrfs.nix
@@ -11,9 +11,9 @@
   services.snapper = {
     snapshotInterval = "*:0/5";
     cleanupInterval = "1h";
-    configs = let
-      mkConfig = subvol: {
-        SUBVOLUME = subvol;
+    configs = {
+      data = {
+        SUBVOLUME = "/persistent/data";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
         TIMELINE_MIN_AGE = 2 * 3600;
@@ -23,9 +23,17 @@
         TIMELINE_LIMIT_MONTHLY = 10;
         TIMELINE_LIMIT_YEARLY = 10;
       };
-    in {
-      data = mkConfig "/persistent/data";
-      cache = mkConfig "/persistent/cache";
+      cache = {
+        SUBVOLUME = "/persistent/cache";
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_MIN_AGE = 30 * 60;
+        TIMELINE_LIMIT_HOURLY = 4;
+        TIMELINE_LIMIT_DAILY = 3;
+        TIMELINE_LIMIT_WEEKLY = 2;
+        TIMELINE_LIMIT_MONTHLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
     };
   };
 }
