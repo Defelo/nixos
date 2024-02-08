d708576d957610890247aeb50154b95e345ba870
Defelo <elodef42@gmail.com>
2024-02-08 16:55:51 +0100

    Update pcsclite

--- system/services.nix
+++ system/services.nix
@@ -1,28 +1,9 @@
-{
-  pkgs,
-  pkgs-old,
-  ...
-}: {
-  imports = [
-    ./pcscd.nix
-  ];
-
-  # services.printing.enable = true;
-
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # services.openssh.enable = true;
-
+{pkgs, ...}: {
   virtualisation.podman = {
     enable = true;
   };
 
   services.pcscd.enable = true;
-  services.pcscd.package = pkgs-old.pcscliteWithPolkit;
 
   programs.ssh.startAgent = false;
 
