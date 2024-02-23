ecf543e55a2ef2fc86d343686a08836c256d8b20
Defelo <elodef42@gmail.com>
2024-02-24 00:01:08 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'fenix':
        'github:nix-community/fenix/846fc5ddb810c36411de6587384bef86c2db5127' (2024-02-20)
      → 'github:nix-community/fenix/a0f0f781683e4e93b61beaf1dfee4dd34cf3a092' (2024-02-23)
    • Updated input 'fenix/nixpkgs':
        'github:nixos/nixpkgs/b98a4e1746acceb92c509bc496ef3d0e5ad8d4aa' (2024-02-18)
      → 'github:nixos/nixpkgs/0e74ca98a74bc7270d28838369593635a5db3260' (2024-02-21)
    • Updated input 'fenix/rust-analyzer-src':
        'github:rust-lang/rust-analyzer/2223b4fa71e543ac6dd1abd4770a69fab8dbdec1' (2024-02-19)
      → 'github:rust-lang/rust-analyzer/1f54f714caf7d38aaab9ac49ba25caa5abd52937' (2024-02-22)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/a54e05bc12d88ff2df941d0dc1183cb5235fa438' (2024-02-18)
      → 'github:nix-community/home-manager/0e0e9669547e45ea6cca2de4044c1a384fd0fe55' (2024-02-22)
    • Updated input 'nix-vscode-extensions':
        'github:nix-community/nix-vscode-extensions/64ff3452483eb29ad15e7e1a943831629368ae90' (2024-02-20)
      → 'github:nix-community/nix-vscode-extensions/7f3ec76468ec5b580c662901a97b85ad8272340c' (2024-02-23)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/b98a4e1746acceb92c509bc496ef3d0e5ad8d4aa' (2024-02-18)
      → 'github:NixOS/nixpkgs/cbc4211f0afffe6dfd2478a62615dd5175a13f9a' (2024-02-23)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/ffed177a9d2c685901781c3c6c9024ae0ffc252b' (2024-02-18)
      → 'github:Mic92/sops-nix/f6b80ab6cd25e57f297fe466ad689d8a77057c11' (2024-02-21)

--- system/nixpkgs.nix
+++ system/nixpkgs.nix
@@ -22,17 +22,6 @@ import nixpkgs {
         pkgs = prev;
       })
     (final: prev: {
-      linuxPackages_latest = prev.linuxPackages_latest.extend (lpfinal: lpprev: {
-        rtl8821ce = lpprev.rtl8821ce.overrideAttrs ({src, ...}: {
-          version = "${lpprev.kernel.version}-unstable-2024-01-20";
-          src = final.fetchFromGitHub {
-            inherit (src) owner repo;
-            rev = "66983b69120a13699acf40a12979317f29012111";
-            hash = "sha256-Zxb9cOgP67QdCeTNEme0tAsBqd9j/2k+gcE1QKkUQU4=";
-          };
-        });
-      });
-
       obsidian =
         (prev.obsidian.overrideAttrs ({src, ...}: rec {
           version = "1.5.8";
