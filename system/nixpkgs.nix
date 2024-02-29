146e087778f450ed28b36e044f8acae1b210b122
Defelo <elodef42@gmail.com>
2024-02-29 17:05:24 +0100

    Remove obsidian overlay

--- system/nixpkgs.nix
+++ system/nixpkgs.nix
@@ -21,16 +21,5 @@ import nixpkgs {
         inherit inputs;
         pkgs = prev;
       })
-    (final: prev: {
-      obsidian =
-        (prev.obsidian.overrideAttrs ({src, ...}: rec {
-          version = "1.5.8";
-          src = prev.fetchurl {
-            url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/obsidian-${version}.tar.gz";
-            hash = "sha256-oc2iA2E3ac/uUNv6unzfac5meHqQzmzDVl/M9jNpS/M=";
-          };
-        }))
-        .override {electron = prev.electron_28;};
-    })
   ];
 }
