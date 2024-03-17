436bc21ff99dbe85c39f1c1d27d859e9b6e60926
Defelo <elodef42@gmail.com>
2024-03-17 19:54:15 +0100

    Fix nix build

--- home/git.nix
+++ home/git.nix
@@ -15,7 +15,7 @@
     extraConfig = {
       diff.submodule = "log";
       diff.sopsdiffer.textconv = let
-        conf = builtins.toFile ".sops.yaml" (builtins.toJSON {
+        conf = builtins.toFile "sops.yaml" (builtins.toJSON {
           creation_rules = [{key_groups = [{pgp = ["61303BBAD7D1BF74EFA44E3BE7FE2087E4380E64"];}];}];
         });
       in "${pkgs.sops}/bin/sops --config ${conf} -d";
