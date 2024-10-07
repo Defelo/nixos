61702e127d29a5c9c53928264304aaeb0a6a4437
Defelo <mail@defelo.de>
2024-10-08 01:18:23 +0200

    Add secure boot support to libvirt

--- system/virt.nix
+++ system/virt.nix
@@ -1,5 +1,22 @@
-{
-  virtualisation.libvirtd.enable = true;
+{pkgs, ...}: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          })
+          .fd
+        ];
+      };
+    };
+  };
+
   programs.dconf.enable = true;
 
   # virtualisation.waydroid.enable = true;
