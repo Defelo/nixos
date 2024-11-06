02e7a9ea3aae5c231e943cd438e6079651222375
Defelo <mail@defelo.de>
2024-11-06 15:27:39 +0100

    Enable nested virtualization

--- system/virt.nix
+++ system/virt.nix
@@ -17,6 +17,12 @@
     };
   };
 
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
+
   programs.dconf.enable = true;
 
   # virtualisation.waydroid.enable = true;
